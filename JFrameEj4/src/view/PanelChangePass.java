5d34d6330b31539f514b6065ea3b5f12d3b4deb3
SrPollo25 <satan@192.168.1.40>
2022-04-12 15:07:45 +0200

    idk

--- JFrameEj4/src/view/PanelChangePass.java
+++ JFrameEj4/src/view/PanelChangePass.java
@@ -30,6 +30,7 @@ public class PanelChangePass extends javax.swing.JPanel {
             modelCombo = new DefaultComboBoxModel();
             comboUsers.setModel(modelCombo);
             chargeUsers();
+            
         }
     }
 
